b682a01b77d6f788aa486aec90942efd7a04d8ed
test <nikhilsys001@gmail.com>
2026-07-04 18:09:23 +0530

    Prep for Render deploy

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -15,7 +15,7 @@ const router = express.Router();
 const cookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === 'production',
-  sameSite: 'strict',
+  sameSite: 'none' ,
   maxAge: 7 * 24 * 60 * 60 * 1000,
 };
 
